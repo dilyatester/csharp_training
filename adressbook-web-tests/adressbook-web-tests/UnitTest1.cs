9b0667fc4c10b38a49711d4e1a9fa98e1baa4051
dilya.shafigullina <dilya.shafigullina@DSRKZN535>
2021-03-17 23:25:59 +0300

    ифсл

--- adressbook-web-tests/adressbook-web-tests/UnitTest1.cs
+++ adressbook-web-tests/adressbook-web-tests/UnitTest1.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace adressbook_web_tests
 {
     [TestClass]
-    public class UnitTest11
+    public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
