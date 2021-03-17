c750892956243944e805055ce734a4a816cea13f
dilya.shafigullina <dilya.shafigullina@DSRKZN535>
2021-03-17 23:20:13 +0300

    Проект создан

---
+++ adressbook-web-tests/adressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace adressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
