3ec2fa03014da74f1194682b21fe4221a1c75739
dilyatester <80853480+dilyatester@users.noreply.github.com>
2021-03-17 23:24:57 +0300

    Update UnitTest1.cs

--- adressbook-web-tests/adressbook-web-tests/UnitTest1.cs
+++ adressbook-web-tests/adressbook-web-tests/UnitTest1.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace adressbook_web_tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest11
     {
         [TestMethod]
         public void TestMethod1()
