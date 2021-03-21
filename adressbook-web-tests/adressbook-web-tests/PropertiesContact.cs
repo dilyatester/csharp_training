b20a0139a1fc9eb0ff8f62f76533b5726ecdf016
dilyatester <Dilya.shafigullina@norbit.ru>
2021-03-21 23:53:54 +0300

    Создан новый проект ContactCreationRests.

---
+++ adressbook-web-tests/adressbook-web-tests/PropertiesContact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdressbookTests
+{
+    class PropertiesContact
+    {
+        private string firstname;
+        private string lastname;
+
+        public PropertiesContact(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
